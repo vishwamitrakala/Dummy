8dae57fa7a5d7f526a4680c2c89714d580645175
vishwamitra <vishwamitrakala@gmail.com>
2017-04-21 12:14:17 +0200

    avcasdf

---
+++ Sample/test.java
@@ -0,0 +1,14 @@
+package hackerearth;
+
+import java.math.BigInteger;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BigInteger big = BigInteger.valueOf((long) Math.pow(2, 64));	
+		System.out.println(big);
+	}
+
+}
+//Printing large values using BigInteger
