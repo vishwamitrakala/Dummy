5c97d949672b072d49411e46c6d1b6a32ac3fcce
vishwamitrakala <vishwamitrakala@gmail.com>
2019-11-04 02:37:08 +0530

    code changed for build test

--- src/hackerearth/TestBuild.java
+++ src/hackerearth/TestBuild.java
@@ -5,6 +5,7 @@ public class TestBuild {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Guys");
+		System.out.println("Hello Guys");
 	}
 
 }
